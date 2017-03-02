79de7e59bcbd7fdef1fcc66c5208ba83db061c1f
VladimirA97 <vladimira@TTC.CAT>
2017-03-02 10:38:40 +0100

    ESCAPE_DONE

--- sdl/main.cpp
+++ sdl/main.cpp
@@ -18,32 +18,44 @@ int main(int argc, char* argv[])
 
 	for (;;)
 	{
-		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-		SDL_RenderClear(renderer);
-		SDL_RenderPresent(renderer);
+	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+	SDL_RenderClear(renderer);
+	//SDL_RenderPresent(renderer);
 
-		SDL_Rect r;
-		r.x = 215;
-		r.y = 125;
-		r.w = 220;
-		r.h = 220;
+	SDL_Rect r;
+	r.x = 215;
+	r.y = 125;
+	r.w = 220;
+	r.h = 220;
 
-		SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
-		SDL_RenderFillRect(renderer, &r);
-		SDL_RenderPresent(renderer);
+	SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+	SDL_RenderFillRect(renderer, &r);
+	SDL_RenderPresent(renderer);
 
+	const Uint8* state = SDL_GetKeyboardState(NULL);
+	SDL_Event event;
+	while (SDL_PollEvent(&event))
+	{
+		if (state[SDL_SCANCODE_ESCAPE])
+		{
+			SDL_Quit();
+		}
+	}
+	/*
 		SDL_Event event;
 		while (SDL_PollEvent(&event))
 		{
-			if (event.type == SDL_Scancode(SDLK_ESCAPE))
+			if (event.type == SDL_KEYDOWN)
 			{
-				break;
+				if (event.key.keysym.sym == SDLK_ESCAPE)
+				{
+					SDL_Quit();
+				}
 			}
 		}
-	
+		*/
 	}
-
-	SDL_Quit();
+	
 
 	return(EXIT_SUCCESS);
 }
