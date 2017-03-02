6a3d6ecb5a7b8059e995d0b8844fec5d5b2df135
VladimirA97 <avladimirp7@gmail.com>
2017-03-02 16:04:21 +0100

    FISRT MOVEMENT

--- sdl/main.cpp
+++ sdl/main.cpp
@@ -7,6 +7,9 @@ using namespace std;
 #pragma comment(lib, "x86/SDL2.lib")
 #pragma comment(lib, "x86/SDL2main.lib")
 
+const int WINDOW_WIDTH = 640;
+const int WINDOW_HEIGHT = 480;
+
 int main(int argc, char* argv[])
 {
 	SDL_Window* window;
@@ -14,48 +17,68 @@ int main(int argc, char* argv[])
 
 	SDL_Init(SDL_INIT_EVERYTHING);
 
-	SDL_CreateWindowAndRenderer(640, 480, SDL_WINDOW_RESIZABLE, &window, &renderer);
+	SDL_CreateWindowAndRenderer(WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE, &window, &renderer);
+
+	SDL_Rect rectngl;
+	rectngl.x = 215;
+	rectngl.y = 125;
+	rectngl.w = 220;
+	rectngl.h = 220;
 
 	for (;;)
 	{
-	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-	SDL_RenderClear(renderer);
-	//SDL_RenderPresent(renderer);
+		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+		SDL_RenderClear(renderer);
+		//SDL_RenderPresent(renderer);
 
-	SDL_Rect r;
-	r.x = 215;
-	r.y = 125;
-	r.w = 220;
-	r.h = 220;
+		SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+		SDL_RenderFillRect(renderer, &rectngl);
+		SDL_RenderPresent(renderer);
 
-	SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
-	SDL_RenderFillRect(renderer, &r);
-	SDL_RenderPresent(renderer);
-
-	const Uint8* state = SDL_GetKeyboardState(NULL);
-	SDL_Event event;
-	while (SDL_PollEvent(&event))
-	{
-		if (state[SDL_SCANCODE_ESCAPE])
-		{
-			SDL_Quit();
-		}
-	}
-	/*
 		SDL_Event event;
 		while (SDL_PollEvent(&event))
 		{
 			if (event.type == SDL_KEYDOWN)
 			{
-				if (event.key.keysym.sym == SDLK_ESCAPE)
+				switch (event.key.keysym.sym)
 				{
+				case SDLK_ESCAPE:
 					SDL_Quit();
+					break;
+				case SDLK_LEFT:
+					rectngl.x -= 25;
+					break;
+				case SDLK_RIGHT:
+					rectngl.x += 25;
+					break;
+				case SDLK_UP:
+					rectngl.y -= 25;
+					break;
+				case SDLK_DOWN:
+					rectngl.y += 25;
+					break;
 				}
+				/*
+				if (rectngl.x == WINDOW_WIDTH)
+				{
+					rectngl.x -= 215;
+				}
+				if (rectngl.x == 0)
+				{
+					rectngl.x += 215;
+				}
+				if (rectngl.y == WINDOW_HEIGHT)
+				{
+					rectngl.y -= 125;
+				}
+				if (rectngl.y == 0)
+				{
+					rectngl.y += 125;
+				}
+				*/
 			}
 		}
-		*/
 	}
-	
 
 	return(EXIT_SUCCESS);
 }
